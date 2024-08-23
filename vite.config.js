735fd74ccabc1b733f6f3fc0e30a9fb933910828
Karimtarek011420 <karimtarek011420@gmail.com>
2024-08-23 15:53:20 +0300

    initial commi33t

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   server: {
     proxy: {
       "/API": {
         target: "https://health.code-faster.giize.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/API/, ""),
+        secure: false, // Add this if you're dealing with a self-signed SSL certificate
+        // No rewrite necessary if you want to keep '/API' in the proxied URL
       },
     },
   },
   plugins: [react()],
 });
-
